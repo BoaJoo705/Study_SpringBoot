a448cb5d92a636f791a32bce09fbfe03f0de5499
JooBoa <joocong2265@gmail.com>
2021-09-27 19:25:11 +0900

    Chapter 04 JPA 퀵스타트(P.238까지) 3 JPA API 이해 ( 3.1 EntityManagerFactory 와 EntityManager 이해하기 / 3.2 영속성 컨텍스트와 엔티티 상태 / 3.3 영속성 컨텍스트와 SQL 저장소 이해하기 )

--- Chapter04/src/main/java/com/rubypaper/JPAClient.java
+++ Chapter04/src/main/java/com/rubypaper/JPAClient.java
@@ -1,5 +1,7 @@
 package com.rubypaper;
+
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -26,6 +28,10 @@ public class JPAClient {
 			// Transaction 시작
 			tx.begin();
 			
+			// 수정할 게시글 조회
+//			Board board = em.find(Board.class, 1L);
+//			board.setTitle("검색한 게시글의 제목 수정");
+			
 			Board board = new Board();
 			board.setTitle("JPA 제목");
 			board.setWriter("관리자");
@@ -35,6 +41,28 @@ public class JPAClient {
 			
 			// 글 등록
 			em.persist(board);
+			
+			// 삭제할 게시글 조회 
+//			Board board1 =em.find(Board.class, 1L);
+//			board1.setSeq(1L);
+			
+			// 게시글 삭제
+//			board1.setSeq(1L);
+//			em.remove(board1);
+//			
+			
+			// Transaction commit
+			tx.commit();
+			
+			// Transaction 시작
+			tx.begin();
+			
+			// 글 목록 조회 
+			String jpql = "select b from Board b order by b.seq desc";
+			List<Board> boardList = em.createQuery(jpql, Board.class).getResultList();
+			for(Board brd : boardList) {
+				System.out.println("---> " + brd.toString());
+			}
 			
 			// Transaction commit
 			tx.commit();
